b5862222689c3f6a22d201a90f35f6437cdee911
Hien Luong Tran Hy <hyhien@gmail.com>
2025-03-14 16:15:13 +0700

    update handle start-stop

--- Buoi05/stopWatch/app/index.tsx
+++ Buoi05/stopWatch/app/index.tsx
@@ -1,13 +1,49 @@
-import { Text, View, StyleSheet, ScrollView} from "react-native";
+import { useState, useRef } from "react";
+import { Text, View, StyleSheet, ScrollView, TouchableOpacity} from "react-native";
+import formatTime from "minutes-seconds-milliseconds";
 
 export default function Index() {
+  const [timeElapsed, setTimeElapsed] = useState<number>(0);
+  const [isRunning, setIsRunning] =  useState<boolean>(false);
+  const [laps, setLaps] = useState<number[]>([]);
+  
+  const intervalRef = useRef<number | null>(null);
+  const startTimeRef = useRef<number | null>(null);
+
+  const handleStartPress = () => {
+    if(isRunning) {
+      clearInterval(intervalRef.current!);
+      setIsRunning(false);
+    } else{
+      startTimeRef.current = Date.now();
+      intervalRef.current = setInterval(() => {
+        setTimeElapsed(Date.now() - startTimeRef.current!);
+      }, 100);
+      setIsRunning(true);
+    }
+  }
+
+  const startStopButton = () => {
+    let mybtnStyle = isRunning ? styles.StopButton : styles.startButton;
+    let mybtnText = isRunning ? 'Stop' : 'Start';
+    return (
+      <TouchableOpacity style = {[styles.button, mybtnStyle]} onPress={handleStartPress}>
+        <Text>{mybtnText}</Text>
+      </TouchableOpacity>
+    );
+  }
+
   return (
     <View style = {styles.container}>
       <View style = {[styles.header]}>
         <View style = {[styles.timerWrapper]}>
-          <Text style = {styles.timer}>00:00:00</Text>
+          <Text style = {styles.timer}>{formatTime(timeElapsed)}</Text>
         </View>
         <View style = {[styles.buttonWrapper]}>
+          { startStopButton() }
+          <TouchableOpacity style = {[styles.button, styles.lap]}>
+            <Text>LAP</Text>
+          </TouchableOpacity>
         </View>
       </View>
       <View style = {[styles.footer]} >
@@ -20,7 +56,7 @@ export default function Index() {
 const styles = StyleSheet.create({
   container : {
     flex : 1,
-    backgroundColor : '#9E9E9E',
+    backgroundColor : '#EEEE9E',
     alignItems : 'stretch'
   },
   header :{
