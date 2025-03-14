ae20fb84f840b25e1cbcd087f655c77d10fe81a2
Hien Luong Tran Hy <hyhien@gmail.com>
2025-03-15 05:36:04 +0700

    Handle event

--- Buoi05/stopWatch/app/index.tsx
+++ Buoi05/stopWatch/app/index.tsx
@@ -3,11 +3,11 @@ import { Text, View, StyleSheet, ScrollView, TouchableOpacity} from "react-nativ
 import formatTime from "minutes-seconds-milliseconds";
 
 export default function Index() {
-  const [timeElapsed, setTimeElapsed] = useState<number>(0);
+  const [elapsedTime, setElapsedTime] = useState<number>(0);
   const [isRunning, setIsRunning] =  useState<boolean>(false);
   const [laps, setLaps] = useState<number[]>([]);
   
-  const intervalRef = useRef<number | null>(null);
+  const intervalRef = useRef<Timeout | null>(null);
   const startTimeRef = useRef<number | null>(null);
 
   const handleStartPress = () => {
@@ -15,11 +15,12 @@ export default function Index() {
       clearInterval(intervalRef.current!);
       setIsRunning(false);
     } else{
+      setIsRunning(true);
+      setLaps([]);
       startTimeRef.current = Date.now();
       intervalRef.current = setInterval(() => {
-        setTimeElapsed(Date.now() - startTimeRef.current!);
-      }, 100);
-      setIsRunning(true);
+        setElapsedTime(Date.now() - startTimeRef.current!);
+      }, 10);
     }
   }
 
@@ -33,21 +34,37 @@ export default function Index() {
     );
   }
 
+  const handlePressLap = () => {
+    setLaps((laps) => [...laps, elapsedTime]);
+  };
+
+  const displayLaps = () => {
+    return laps.map((time, index) => {
+      return (
+        <View key={index} style={styles.lap}>
+          <Text style={styles.lapText}>Lap #{index + 1}</Text>
+          <Text style={styles.lapText}>{formatTime(time)}</Text>
+        </View>
+      );
+    });
+  };
+
   return (
     <View style = {styles.container}>
       <View style = {[styles.header]}>
         <View style = {[styles.timerWrapper]}>
-          <Text style = {styles.timer}>{formatTime(timeElapsed)}</Text>
+          <Text style = {styles.timer}>{formatTime(elapsedTime)}</Text>
         </View>
         <View style = {[styles.buttonWrapper]}>
           { startStopButton() }
-          <TouchableOpacity style = {[styles.button, styles.lap]}>
+          <TouchableOpacity style = {[styles.button, styles.lap]}  onPress={handlePressLap}>
             <Text>LAP</Text>
           </TouchableOpacity>
         </View>
       </View>
       <View style = {[styles.footer]} >
         <ScrollView style={{flex:1, backgroundColor:'#E0E0E0'}}>
+          {displayLaps()}
         </ScrollView>
       </View>
     </View>
@@ -56,7 +73,7 @@ export default function Index() {
 const styles = StyleSheet.create({
   container : {
     flex : 1,
-    backgroundColor : '#EEEE9E',
+    backgroundColor : '#A0A08E',
     alignItems : 'stretch'
   },
   header :{
